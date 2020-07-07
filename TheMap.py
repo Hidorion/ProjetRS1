71aed0ed66c8178536265f39d6b27137508dfbc2
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-07-07 14:03:36 +0200

    V1.1

--- TheMap.py
+++ TheMap.py
@@ -1,33 +1,29 @@
+import random
+
 #a quel y / b quel x / c par quel caractère / d doit être la map à changer
 def pointchanger (y,x,sample,map):
     map[y][x] = sample
 
 def CantMoveSpots(d): #Squelette immuable de la map
-    for i in range(5):
+    for i in range(3):
         linechanger(0,30,i,"M",d)
-        linechanger(35,82,i,"M",d)
+        linechanger(35,100,i,"M",d)
         linechanger(30,35,i,"S",d)
-        linechanger(7,32,i+24,"░",d)
-    for j in range(5, 16):
+    for j in range(3,25):
         linechanger(0,7,j,"M",d)
-        linechanger(30,35,j,"S",d)
-        linechanger(75,82,j,"M",d)
-        linechanger(32,37,(j +13),"S",d)
-        linechanger(29,30,j,"░",d)
-        linechanger(35,36,j,"░",d)
-    for k in range(15, 32):    
-        linechanger(0,7,k,"M",d)
-        linechanger(75,82,k,"M",d)
-        linechanger(37,75,k,"░",d)
-    for l in range(29,32):
-        linechanger(7,82,l,"~",d)
-    pointchanger(15,36,"░",MyListY)
+        linechanger(93,100,j,"M",d)
+        linechanger(92,93,j,"T",d)
+    for k in range (25,30):
+        linechanger(0,100,k,"~",d)
+    for sand in range (20,25):
+        linechanger(8,92,sand,"░",d)
+        linechanger(35,50,sand,"S",d)
 
 
 MyListY = [] 
-for y in range(32): 
+for y in range(30): 
     MySubList = [] 
-    for x in range(82): 
+    for x in range(100): 
         MySubList.append(" ") 
     MyListY.append(MySubList)
 
@@ -49,8 +45,13 @@ def linechanger (depart,arrive,Ligne,sample,map):
         map[Ligne][j] = sample
         
 
-CantMoveSpots(MyListY)
-pointchanger(28,65,"☻",MyListY)
+CantMoveSpots(MyListY) #Entrées des points visitables
+pointchanger(24,85,"☻",MyListY)
+#pointchanger(random.randint(20,26),74,"۩",MyListY)
+pointchanger(2,random.randint(50,90),"○",MyListY) #Position César Code
+pointchanger(random.randint(10,16),92,"◌",MyListY) #Position Sphinx Nombre Mystérieux
+#pointchanger(random.randint(20,25),random.randint(8,14),"●",MyListY) #Position Fizz Buzz
+
 def printmap():
     for Base_x in MyListY:
         print("".join(Base_x))
@@ -60,8 +61,14 @@ def printmap():
 Eau potable = S
 Eau de mer = ~
 Montagne = M
-Portes = ●
-Objets = ○
 Sable = ░
 Plaine = " "
+Porte 1 César = ○
+Porte 2 ????  = ◌
+Porte 3 FB-ZZ = ●
+Objet consommable = ©
+Objet récupérable = ʘ
+Grande Porte = ۩
+Arbre = T
+Pont = "="
 """
