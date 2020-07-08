e8b6538ca8d1ee4d4cbef203b803c9a2b56f178b
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-07-08 11:50:02 +0200

    minor changes

---
+++ Inventory.py
@@ -0,0 +1,85 @@
+import random
+PlayerInventory = []*10
+
+
+BaseItems = {
+    "2.1" : "Laptop and Charger",
+    "Usable" : False,
+    "2.2" : "Pocket Map",
+    "Usable" : True,
+    "2.3" : "Swiss army Knife",
+    "Usable" : True,
+    "2.4" : "Flask",
+    "Usable" : True,
+    "NbUse" : 4,
+    }
+
+LootableItems = {
+    "1.1" : "Watermelon",
+    "Usable" : True,
+    "Energy" : 0,
+    "Starve" : 2,
+    "Thurst" : 8,
+    "NbUse"  : 4,
+    "1.2" : "Filthy Water",
+    "Usable" : True,
+    "Energy" : random.randint(-6,-2),
+    "Starve" : 0,
+    "Thurst" : 0,
+    "1.3" : "Clear Water",
+    "Usable" : True,
+    "Energy" : 0,
+    "Starve" : 0,
+    "Thurst" : 12,
+    "1.4" : "Camp Bed",
+    "Usable" : True,
+    "Energy" : 2,
+    "Starve" : 0,
+    "Thurst" : 1,
+    "1.5" : "Aloe Vera",
+    "Usable" : True,
+    "Energy" : 8,
+    "Starve" : 1,
+    "Thurst" : 0,
+    "1.6" : "Agrum",
+    "Usable" : True,
+    "Energy" : 4,
+    "Starve" : -1,
+    "Thurst" : -1,
+    "1.7" : "Red Berries",
+    "Usable" : True,
+    "Energy" : 0,
+    "Starve" : 4,
+    "Thurst" : 1,
+    "1.8" : "Banana",
+    "Usable" : True,
+    "Energy" : 0,
+    "Starve" : 10,
+    "Thurst" : 0,
+    "1.1" : "Weird Fruit",
+    "Usable" : True,
+    "Energy" : -3,
+    "Starve" : 10,
+    "Thurst" : -1,
+    }
+    
+Keys = {
+    "3.1" : "Bronze Key",
+    "Usable" : True,
+    "Get" : False,
+    "3.2" : "Silver Key",
+    "Usable" : True,
+    "Get" : False,
+    "3.3" : "Golden Key",
+    "Usable" : True,
+    "Get" : False,
+    "3.4" : "USB Key",
+    "Usable" : True,
+    "Get" : False, 
+    }
+
+IslandInventory = {
+    "BaseItems" : BaseItems,
+    "LootableItems" : LootableItems,
+    "Keys" : Keys
+}
