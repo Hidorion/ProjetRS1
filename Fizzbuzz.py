8bc8e14c098c4b79ab9582601de5d0f05a46d3a3
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-07-12 15:48:46 +0200

    FizzBuzz is getting better

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,24 +1,93 @@
 import random
 
 
-"""
-Player = "Alex"
-ListOfPlayers = ["Singe Agile", "Singe Marrant", "Singe Sage", "Singe Grand", "Singe Mince", "Chef Singe", Player ]
-maxi = 0
-while maxi <= len(ListOfPlayers):
-    
-    test = maxi + 1
-    if test %3 == 0 and test %5 == 0 :
-        print(f"{ListOfPlayers[j]} dit : Fizz Buzz !!")
-    elif test %3 == 0:
-        print(f"{ListOfPlayers[j]} dit : Fizz")
-    elif test %5 == 0:
-        print(f"{ListOfPlayers[j]} dit : Buzz")
-    else:
-        print(f"{ListOfPlayers[j]} dit : {test}")
-    if test >= maxi:
-        maxi = test
-print("resdrgq")   
 
-"""
+Player = "Alex"
+ListOfPlayers = ["Singe Agile", "Singe Marrant", "Singe Sage", "Singe Grand", "Singe Mince", "Chef Gorille", Player]
+PosInList = 0
+
+while Player not in ListOfPlayers[0]:
+    if Player not in ListOfPlayers:
+        break
+    else:
+        for j in range(50):
+            Turn = j + 1
+            if PosInList > len(ListOfPlayers)-1:
+                PosInList = 0
+            if Turn %3 == 0 and Turn %5 == 0 :
+                if "Singe" in ListOfPlayers[PosInList]:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 31:
+                        print(f"{ListOfPlayers[PosInList]} dit : Fizz Buzz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+                else:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 71:
+                        print(f"{ListOfPlayers[PosInList]} dit : Fizz Buzz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+                    
+            elif Turn %3 == 0:
+                if "Singe" in ListOfPlayers[PosInList]:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 31:
+                        print(f"{ListOfPlayers[PosInList]} dit : Fizz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+                else:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 71:
+                        print(f"{ListOfPlayers[PosInList]} dit : Fizz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+            elif Turn %5 == 0:
+                if "Singe" in ListOfPlayers[PosInList]:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 31:
+                        print(f"{ListOfPlayers[PosInList]} dit : Buzz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+                else:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 71:
+                        print(f"{ListOfPlayers[PosInList]} dit : Buzz !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+            else:
+                if "Singe" in ListOfPlayers[PosInList]:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 31:
+                        print(f"{ListOfPlayers[PosInList]} dit : {Turn} !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+                else:
+                    CheckIfCorrect = random.randint(1,100)
+                    print(CheckIfCorrect)
+                    if CheckIfCorrect < 71:
+                        print(f"{ListOfPlayers[PosInList]} dit : {Turn} !!")
+                    else:
+                        print(f"{ListOfPlayers[PosInList]} se trompe, la hooonte")
+                        Delete = ListOfPlayers.pop(PosInList)
+            PosInList = PosInList + 1
+            print(ListOfPlayers)
+    print("agrandir range")  
+print("C'est gagné")
+print("Blablabla")
+
+
     
