170aa61cc69d537446f1ff2411d2d3f5492428e9
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-07-12 13:46:54 +0200

    fizzbuzz is broken

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,8 +1,24 @@
-import cesar
+import random
 
 
-
-Player = cesar.PlayersName
+"""
+Player = "Alex"
 ListOfPlayers = ["Singe Agile", "Singe Marrant", "Singe Sage", "Singe Grand", "Singe Mince", "Chef Singe", Player ]
+maxi = 0
+while maxi <= len(ListOfPlayers):
+    
+    test = maxi + 1
+    if test %3 == 0 and test %5 == 0 :
+        print(f"{ListOfPlayers[j]} dit : Fizz Buzz !!")
+    elif test %3 == 0:
+        print(f"{ListOfPlayers[j]} dit : Fizz")
+    elif test %5 == 0:
+        print(f"{ListOfPlayers[j]} dit : Buzz")
+    else:
+        print(f"{ListOfPlayers[j]} dit : {test}")
+    if test >= maxi:
+        maxi = test
+print("resdrgq")   
 
-print(ListOfPlayers)
+"""
+    
